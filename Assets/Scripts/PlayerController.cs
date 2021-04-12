3a9298cb73c678af2d5195a8403686378ad2369d
dayzline <69827441+dayzline@users.noreply.github.com>
2021-04-11 19:29:55 -0700

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rbPlayer;
+    public float gravMod;
+    public float jumpMod;
+    private bool onGround = true;
+
+    void Start()
+    {
+       rbPlayer = GetComponent<Rigidbody>();
+        Physics.gravity *= gravMod;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool spaceDown = Input.GetKeyDown(KeyCode.Space);
+        if (spaceDown)
+        {
+            rbPlayer.AddForce(Vector3.up * jumpMod, ForceMode.Impulse);
+            onGround = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        onGround = true;
+
+    }
+}
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject obsPrefab;
+    private Vector3 spawn = new Vector3(15, 0, 0);
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        InvokeRepeating("SpawnObs", 2, 2);
+    }
+
+    void SpawnObs()
+    {
+         Instantiate(obsPrefab, spawn, obsPrefab.transform.rotation);
+    }
+}
