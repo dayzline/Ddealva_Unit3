eb3848d6b0e20d25089c55e4ff5088419723c706
dayzline <69827441+dayzline@users.noreply.github.com>
2021-04-21 21:35:42 -0700

    Finished Lesson C

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,20 +10,25 @@ public class PlayerController : MonoBehaviour
     private bool onGround = true;
     public bool gameOver = false;
 
+    private Animator animPlayer;
+
     void Start()
     {
        rbPlayer = GetComponent<Rigidbody>();
         Physics.gravity *= gravMod;
+
+        animPlayer = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         bool spaceDown = Input.GetKeyDown(KeyCode.Space);
-        if (spaceDown && onGround)
+        if (spaceDown && onGround && !gameOver)
         {
             rbPlayer.AddForce(Vector3.up * jumpMod, ForceMode.Impulse);
             onGround = false;
+            animPlayer.SetTrigger("Jump_trig");
         }
     }
 
@@ -36,6 +41,8 @@ public class PlayerController : MonoBehaviour
         {
             gameOver = true;
             Debug.Log("Game Over");
+            animPlayer.SetBool("Death_b", true);
+            animPlayer.SetInteger("DeathType_int", 2);
         }
     }
 }
